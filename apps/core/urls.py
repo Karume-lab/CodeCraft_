087712d2c76c8a5a86ca5c16e3df05542632609c
Karume-lab <danielkarume@outlook.com>
2023-08-25 10:35:39 +0300

    added styled templates

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import HomeView
+from .views import HomeView, IndexView
 
 app_name = 'core'
 urlpatterns = [
+	path('', IndexView.as_view(), name='index'),
 	path('home/', HomeView.as_view(), name='home'),
 ]
