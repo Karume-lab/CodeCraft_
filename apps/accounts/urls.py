31d201ca4673bc1981136c5a39340242a9351bd9
Karume-lab <danielkarume@outlook.com>
2023-08-26 22:53:48 +0300

    bug fixes
    Co-authored-by: Ephraim Shikanga <ephraimshikanga@gmail.com>

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-
+from .views import CustomLoginView
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('', include('allauth.urls')),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,10 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+from allauth.account.views import LoginView
 
 # Create your views here.
+class CustomLoginView(LoginView):
+    def form_valid(self, form):
+        if self.request.method == 'POST':
+            pass
+        else:
+            pass
