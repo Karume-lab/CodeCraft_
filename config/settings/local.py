423ab63169820e7fdd667c7b29d70f87fbe484cb
Karume-lab <danielkarume@outlook.com>
2023-08-28 14:46:19 +0300

    added config

---
+++ config/settings/local.py
@@ -0,0 +1,37 @@
+# flake8: noqa: F403, F405
+from .base import *
+
+INSTALLED_APPS += ["debug_toolbar"]
+
+MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
+
+# ==============================================================================
+# DATABASES SETTINGS
+# ==============================================================================
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# ==============================================================================
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME", default="postgres_db", cast=str),
+        "USER": config("DB_USER", default="postgres_user", cast=str),
+        "PASSWORD": config("DB_PASS", default="postgres_pass", cast=str),
+        "HOST": config("DB_HOST", default="localhost", cast=str),
+        "PORT": config("DB_PORT", default="5432", cast=str),
+    }
+}
+
+if config("USE_SQLITE", default=False, cast=bool):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
+# EMAIL SETTINGS
+# ==============================================================================
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
