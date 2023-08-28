f9a54cc1e6d4376f8e0efc0d245e74b447354fbd
EphraimShikanga <ephraimshikanga@gmail.com>
2023-08-28 20:41:41 +0300

    fixed themes and added icons in sidebar

--- static/admin/js/theme.js
+++ static/admin/js/theme.js
@@ -1,56 +1,55 @@
-'use strict';
-{
-    window.addEventListener('load', function(e) {
+const modes = document.querySelectorAll('.mode');
+const body = document.body;
+const sidebar = document.getElementById('accordionSidebar');
+const header = document.getElementById('topbarHeader');
+const footer = document.getElementById('bottombarFooter');
 
-        function setTheme(mode) {
-            if (mode !== "light" && mode !== "dark" && mode !== "auto") {
-                console.error(`Got invalid theme mode: ${mode}. Resetting to auto.`);
-                mode = "auto";
-            }
-            document.documentElement.dataset.theme = mode;
-            localStorage.setItem("theme", mode);
-        }
-
-        function cycleTheme() {
-            const currentTheme = localStorage.getItem("theme") || "auto";
-            const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-            if (prefersDark) {
-                // Auto (dark) -> Light -> Dark
-                if (currentTheme === "auto") {
-                    setTheme("light");
-                } else if (currentTheme === "light") {
-                    setTheme("dark");
-                } else {
-                    setTheme("auto");
-                }
-            } else {
-                // Auto (light) -> Dark -> Light
-                if (currentTheme === "auto") {
-                    setTheme("dark");
-                } else if (currentTheme === "dark") {
-                    setTheme("light");
-                } else {
-                    setTheme("auto");
-                }
-            }
-        }
-
-        function initTheme() {
-            // set theme defined in localStorage if there is one, or fallback to auto mode
-            const currentTheme = localStorage.getItem("theme");
-            currentTheme ? setTheme(currentTheme) : setTheme("auto");
-        }
-
-        function setupTheme() {
-            // Attach event handlers for toggling themes
-            const buttons = document.getElementsByClassName("theme-toggle");
-            Array.from(buttons).forEach((btn) => {
-                btn.addEventListener("click", cycleTheme);
-            });
-            initTheme();
-        }
-
-        setupTheme();
-    });
+const sidebarClasses = {
+  default: 'bg-gradient-primary',
+  dark: 'dark',
+  night: 'bg-gradient-dark'
 }
+function updateSidebarColor() {
+  const theme = body.classList.contains('sidebar-toggled')
+  ? body.classList[1]
+  : body.classList[0];
+
+  sidebar.classList.remove(
+	sidebarClasses.default,
+	sidebarClasses.dark,
+	sidebarClasses.night
+  );
+  sidebar.classList.add(sidebarClasses[theme]);
+}
+
+function saveTheme(theme) {
+  localStorage.setItem('theme', theme);
+}
+
+function loadTheme() {
+  const loadedTheme = localStorage.getItem('theme');
+  if (loadedTheme && !body.classList.contains(loadedTheme)) {
+    body.classList.add(loadedTheme);
+    updateSidebarColor();
+  }
+  body.classList.remove('hidden-content');
+}
+
+function handleModeClick(e) {
+  const theme = e.target.dataset.mode;
+    // remove all existing classes
+  modes.forEach(mode => {
+    body.classList.remove(mode.dataset.mode);
+  });
+  body.classList.add(e.target.dataset.mode);
+  updateSidebarColor();
+  saveTheme(theme);
+  }
+  
+  loadTheme();
+  modes.forEach(mode => {
+    mode.addEventListener('click', handleModeClick);  
+    
+  });
+  
+document.addEventListener('DOMContentLoaded', loadTheme);
