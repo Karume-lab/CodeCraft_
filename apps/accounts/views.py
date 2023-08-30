44e34f6469bd8a7f5175feccab1c62ea28c9c255
Karume-lab <danielkarume@outlook.com>
2023-08-30 10:51:04 +0300

    linked additional  pages

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 
 # Create your views here.
+class MyProfileTamplateView(TemplateView):
+    template_name = 'account/profile.html'
