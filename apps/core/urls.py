10f149c2382053f76983b48dc12bb868781ad0e1
Karume-lab <danielkarume@outlook.com>
2023-09-01 18:07:06 +0300

    styled listing tasks into a 3 column grid

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import HomeTemplateView, IndexTemplateView, SearchTemplateView, send_email
-from .views import HomeTemplateView, IndexTemplateView, SearchTemplateView, ShareTemplateView, FeedbackTemplateView, RandomizeDetailView
+from .views import send_email, HomeTemplateView, IndexTemplateView, SearchTemplateView, RandomizeDetailView
+
 
 
 app_name = 'core'
@@ -10,7 +10,5 @@ urlpatterns = [
     path('home/', HomeTemplateView.as_view(), name='home'),
     path('home/search_results/', SearchTemplateView.as_view(), name='search'),
     path('send_email', send_email, name='send_email'),
-    path('home/share/', ShareTemplateView.as_view(), name='share'),
-    path('home/feedback/', FeedbackTemplateView.as_view(), name='feedback'),
-	  path('home/randomize/', RandomizeDetailView.as_view(), name='randomize'),
+    path('home/randomize/', RandomizeDetailView.as_view(), name='randomize'),
 ]
