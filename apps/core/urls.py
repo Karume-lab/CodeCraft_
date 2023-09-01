14190b50fa0abeb2ab355346a41687d6b81113c4
Karume-lab <danielkarume@outlook.com>
2023-09-01 13:32:40 +0300

    adding randomize feature

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeTemplateView, IndexTemplateView, SearchTemplateView, ShareTemplateView, FeedbackTemplateView
+from .views import HomeTemplateView, IndexTemplateView, SearchTemplateView, ShareTemplateView, FeedbackTemplateView, RandomizeDetailView
 
 app_name = 'core'
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('home/search_results/', SearchTemplateView.as_view(), name='search'),
     path('home/share/', ShareTemplateView.as_view(), name='share'),
     path('home/feedback/', FeedbackTemplateView.as_view(), name='feedback'),
+	path('home/randomize/', RandomizeDetailView.as_view(), name='randomize'),
 ]
